fa8aa8ccbc18475c57fcccbdc5523ce8b29dcd12
Vinicius Pinheiro <viniciusps_1998@hotmail.com>
2024-11-07 02:57:36 -0300

    feat: tech filters and update stackData

--- src/components/SkillsCase/index.tsx
+++ src/components/SkillsCase/index.tsx
@@ -1,10 +1,29 @@
-import StyledSkillsCase, { StyledChip } from "./style";
+import StyledSkillsCase, { StyledChip, StyledToggleButtonGroup } from "./style";
 import Stack from "@mui/material/Stack";
 import { stackData } from "../../utils/stackData";
 import { motion } from "framer-motion";
 import { showSkillsVariant } from "../../styles/motionVariants";
+import { ToggleButton } from "@mui/material";
+import { useState } from "react";
+
+enum Filters {
+  ALL = "all",
+  FRONTEND = "frontend",
+  BACKEND = "backend",
+  MOBILE = "mobile",
+  TOOLS = "tools"
+}
 
 const SkillsCase = () => {
+  const [filter, setFilter] = useState(Filters.ALL);
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newFilter: Filters,
+  ) => {
+    setFilter(newFilter);
+  };
+
   return (
     <StyledSkillsCase>
       <motion.div
@@ -13,20 +32,34 @@ const SkillsCase = () => {
         variants={showSkillsVariant}
       >
         <h3 className="title-skills gray-700">Techs que domino</h3>
+        <StyledToggleButtonGroup
+          value={filter}
+          exclusive
+          onChange={handleChange}
+          aria-label="Filter Techs"
+        >
+          <ToggleButton  value={Filters.ALL}>Todas</ToggleButton>
+          <ToggleButton value={Filters.FRONTEND}>Front End</ToggleButton>
+          <ToggleButton value={Filters.BACKEND}>Back End</ToggleButton>
+          <ToggleButton value={Filters.MOBILE}>Mobile</ToggleButton>
+          <ToggleButton value={Filters.TOOLS}>Tools</ToggleButton>
+        </StyledToggleButtonGroup>
         <Stack
           direction="row"
           spacing={0}
           sx={{ flexWrap: "wrap", gap: "3px" }}
         >
-          {stackData.map((tech, index) => (
-            <StyledChip
-              key={index}
-              label={tech.title}
-              variant="outlined"
-              sx={{ "&&": { fontSize: "var(--font-size-skills)" } }}
-              icon={<tech.img color="var(--color-gray-500)" size="1.5em" />}
-            />
-          ))}
+          {stackData.filter((tech) => 
+            filter === Filters.ALL || tech.tags.includes(filter))
+            .map((tech, index) => (
+                <StyledChip
+                  key={index}
+                  label={tech.title}
+                  variant="outlined"
+                  sx={{ "&&": { fontSize: "var(--font-size-skills)" } }}
+                  icon={<tech.img color="var(--color-gray-500)" size="1.5em" />}
+                />
+              ))}
         </Stack>
       </motion.div>
     </StyledSkillsCase>
--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -24,127 +24,192 @@ import {
   SiGraphql,
   SiApollographql,
   SiRedux,
-  SiRadixui
+  SiRadixui,
+  SiVite,
+  SiBitbucket,
+  SiExpo,
+  SiPhp,
+  SiPenpot
 } from "react-icons/si";
 import { GrReactjs } from "react-icons/gr";
 import { FaNodeJs, FaAngular } from "react-icons/fa";
+import { TbBrandReactNative } from "react-icons/tb";
 
 export const stackData = [
   {
     title: "HTML5",
     img: AiFillHtml5,
+    tags: ["frontend"],
   },
   {
     title: "CSS3",
     img: SiCss3,
+    tags: ["frontend"],
   },
   {
     title: "JavaScript",
     img: IoLogoJavascript,
+    tags: ["frontend"],
   },
   {
     title: "React",
     img: GrReactjs,
+    tags: ["frontend"],
+  },
+  {
+    title: "React Native",
+    img: TbBrandReactNative,
+    tags: ["frontend", "mobile"],
   },
   {
     title: "Redux",
     img: SiRedux,
+    tags: ["frontend"],
   },
   {
     title: "AngularJS",
     img: FaAngular,
+    tags: ["frontend"],
   },
   {
     title: "TypeScript",
     img: SiTypescript,
+    tags: ["frontend", "backend", "mobile"],
   },
   {
     title: "React Router",
     img: SiReactrouter,
+    tags: ["frontend"],
   },
   {
     title: "Axios",
     img: SiAxios,
+    tags: ["frontend"],
   },
   {
     title: "JWT",
     img: SiJsonwebtokens,
+    tags: ["backend"],
   },
   {
     title: "Material UI",
     img: SiMui,
+    tags: ["frontend"],
   },
   {
     title: "Radix UI",
     img: SiRadixui,
+    tags: ["frontend"],
   },
   {
     title: "Styled Components",
     img: SiStyledcomponents,
+    tags: ["frontend"],
   },
   {
     title: "Framer Motion",
     img: SiFramer,
+    tags: ["frontend"],
   },
   {
     title: "NodeJs",
     img: FaNodeJs,
+    tags: ["backend"],
   },
   {
     title: "Express",
     img: SiExpress,
+    tags: ["backend"],
   },
   {
     title: "Python",
     img: SiPython,
+    tags: ["backend"],
   },
   {
     title: "Django",
     img: SiDjango,
+    tags: ["backend"],
+  },
+  {
+    title: "PHP",
+    img: SiPhp,
+    tags: ["backend"],
   },
   {
     title: "Apache Kafka",
     img: SiApachekafka,
+    tags: ["backend"],
   },
   {
     title: "GraphQl",
     img: SiGraphql,
+    tags: ["backend"],
   },
   {
     title: "Apollo GraphQL",
     img: SiApollographql,
+    tags: ["backend"],
   },
   {
     title: "Mongoose",
     img: SiMongoose,
+    tags: ["backend"],
   },
   {
     title: "MongoDB",
     img: SiMongodb,
+    tags: ["backend"],
   },
   {
     title: "Postgresql",
     img: SiPostgresql,
+    tags: ["backend"],
   },
   {
     title: "Sqlite",
     img: SiSqlite,
+    tags: ["backend"],
   },
   {
     title: "Git",
     img: SiGit,
+    tags: ["tools"],
   },
   {
     title: "Github",
     img: SiGithub,
+    tags: ["tools"],
+  },
+  {
+    title: "Bitbucket",
+    img: SiBitbucket,
+    tags: ["tools"],
+  },
+  {
+    title: "Vite",
+    img: SiVite,
+    tags: ["frontend", "tools"],
+  },
+  {
+    title: "Expo",
+    img: SiExpo,
+    tags: ["frontend", "mobile", "tools"],
   },
   {
     title: "Figma",
     img: SiFigma,
+    tags: ["tools"],
   },
   {
     title: "Adobe Xd",
     img: SiAdobexd,
+    tags: ["tools"],
+  },
+  {
+    title: "Penpot",
+    img: SiPenpot,
+    tags: ["tools"],
   },
 ];
 
